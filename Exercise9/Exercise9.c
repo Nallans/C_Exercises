40a7cff0a3d692c9195c148d233e739272b72891
Nallans <nallans.code@gmail.com>
2026-07-29 21:39:46 +0200

    Exercises 8 + 9

--- Exercise9/Exercise9.c
+++ Exercise9/Exercise9.c
@@ -2,14 +2,25 @@
 
 int main(void)
 {
-    int secret = 42;
+    int secret = 42, tries = 0;
     int num;
     
-    while(num =! secret)
+    while(num != secret)
     {
-        printf("Guess the number !\n");
+        printf("Guess the number : ");
         scanf("%d", &num);
+        tries++;
+
+        if(num > secret)
+        {
+            printf("Too high !\n");
+        }
+        else
+            printf("Too low !\n");
+        printf("\n");
     }
+    printf( "WOW YOU GUESSED IT !\n"
+            "It took you %d tries !\n", tries);
 
     return(0);
 }
