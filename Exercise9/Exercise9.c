88bceaa2cbb8b1f1c8f71ab6039440a0faca3a5f
Nallans <nallans.code@gmail.com>
2026-07-30 23:02:56 +0200

    Exercise 9 Improved

--- Exercise9/Exercise9.c
+++ Exercise9/Exercise9.c
@@ -2,8 +2,23 @@
 
 int main(void)
 {
+    void game(int tries, int num, int secret);
     int secret = 42, tries = 0, num = 0;
-    
+    char replay = 'y';
+
+    while(replay != 'n')
+    {
+        tries = 0, num = 0;
+        game(tries, num, secret);
+        printf("Play again ? (y/n) : ");
+        scanf(" %c", &replay);
+    }
+
+    return(0);
+}
+
+void game(int tries, int num, int secret)
+{
     while(num != secret)
     {
         printf("Guess the number : ");
@@ -16,10 +31,8 @@ int main(void)
         }
         else if(num < secret)
             printf("Too low !\n");
-        printf("\n");
     }
+    printf("\n");
     printf( "WOW YOU GUESSED IT !\n"
             "It took you %d tries !\n", tries);
-
-    return(0);
 }
