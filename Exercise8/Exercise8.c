c6c053f06cee8d1630840c1e56bd55df5791e967
Nallans <nallans.code@gmail.com>
2026-07-30 23:09:24 +0200

    Exercise 8 Improved

--- Exercise8/Exercise8.c
+++ Exercise8/Exercise8.c
@@ -2,13 +2,14 @@
 
 int main(void)
 {
-    int num, product;
+    int num, product, limit;
 
     printf( "Hello !\n"
-            "Can you give me a positive integer please ? :)\n");
-    scanf("%d", &num);
+            "Can you give me a positive integer please ? :)\n"
+            "Give me also the limit of the table in this format : x x\n");
+    scanf("%d %d", &num, &limit);
 
-    for(int i = 1; i < 11; i++)
+    for(int i = 1; i <= limit; i++)
     {
         product = num * i;
         printf("%d x %d = %d\n", num, i, product);
