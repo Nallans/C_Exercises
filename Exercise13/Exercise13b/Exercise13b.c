8f3a38a5b3326af0cc7d2bb198c2946bbe1f4794
Nallans <nallans.code@gmail.com>
2026-08-06 20:50:53 +0200

    Exercise 13b

--- Exercise13/Exercise13b/Exercise13b.c
+++ Exercise13/Exercise13b/Exercise13b.c
@@ -1,5 +1,39 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int numbers[5] ={1,2,3,4,5};
+    void display(int* numbers);
+    void swap(int* numbers);
+
+    display(numbers);
+
+    swap(numbers);
+
+    printf("\n");
+    display(numbers);
+    printf("\n");
 
 
+    return(0);
+}
+
+void display(int* numbers)
+{
+    for(int i = 0; i < 5; i++)
+    {
+        printf("%d", numbers[i]);
+    }
+}
+
+void swap(int* numbers)
+{
+    int buffer;
+
+    buffer = numbers[0];
+    numbers[0] = numbers[4];
+    numbers[4] = buffer;
+}
 /*
 Exercise 13.2 — Swap two elements in an array
 
