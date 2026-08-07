186f17fc6d9d70d2e4ac5435e84525a8cc9fc26a
Nallans <nallans.code@gmail.com>
2026-08-07 21:24:37 +0200

    Exercise 13, and overdelivered, finally

--- Exercise13/Exercise13.c
+++ Exercise13/Exercise13.c
@@ -1,26 +1,72 @@
 #include <stdio.h>
 
+void display(int* numbers, int length);
+void get_length(int* length);
+void get_numbers(int* numbers, int length);
+void reverse(int* numbers, int length);
+
 int main(void)
 {
-    int numbers[6];
-    int buffer;
+    int length;
+    get_length(&length);
 
-    printf("Give me 6 integers please : ");
+    int numbers[length];
+    get_numbers(numbers, length);
 
-    for(int i = 0; i < 6; i++)
-    {
-        scanf("%d", &numbers[i]);
-        //printf("%d\n", numbers[i]);
-    }
-
-    for(int i = 0; i < 6; i++)
-    {
-        buffer = (numbers + i);
-        *(numbers + i) = *(numbers + (5 - i));
-        *(numbers + (5 - i)) = buffer;
-    }
+    printf("Original array : ");
+    display(numbers, length);
+    reverse(numbers, length);
+    printf("\n");
+    printf("Reversed array : ");
+    display(numbers, length);
+    printf("\n");
 
     return(0);
+}
+
+void display(int* numbers, int length)
+{
+    for(int i = 0; i < length; i++)
+    {
+        printf("%d ", numbers[i]);
+    }
+}
+
+void get_numbers(int* numbers, int length)
+{
+    printf("Please give me %d numbers : ", length);
+
+    for(int i = 0; i < length; i++)
+    {
+        scanf("%d", &numbers[i]);
+    }
+}
+
+void get_length(int* length)
+{
+    printf("Please give me an even length between 2 and 20 : ");
+    scanf("%d", length);
+
+    while((*length < 2 || *length > 20) || *length % 2 != 0)
+    {
+        printf( "That's not what I asked :'( "
+                "Please give me a correct number : ");
+        scanf("%d", length);
+    }
+}
+
+void reverse(int* numbers, int length)
+{
+    int buffer;
+    int end = length - 1;   // Setting the end for more readability and not getting
+                            // a random value outside the array.
+                            
+    for(int i = 0; i < (length / 2); i++)
+    {
+        buffer = numbers[i];
+        numbers[i] = numbers[end - i];
+        numbers[end - i] = buffer;
+    }
 }
 
 /*
