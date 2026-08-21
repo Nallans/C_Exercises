1a24ecab06f1df3c45979b6f85f6874639a85718
Nallans <nallans.code@gmail.com>
2026-08-21 21:01:22 +0200

    Consolidation Set and CS1E1 Wording

---
+++ Consolidation_Set1/Exercise1/CS1E1.c
@@ -0,0 +1,16 @@
+/*
+Exercise 25 — Character counter
+
+Write a program that:
+
+asks for a word,
+asks for a character,
+counts how many times that character appears,
+prints the result.
+
+Example:
+
+Word: banana
+Character: a
+'a' appears 3 times.
+*/
