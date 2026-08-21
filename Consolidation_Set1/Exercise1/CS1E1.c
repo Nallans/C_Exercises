ad95c1267495187ba61a2fdc9bdfcc45c9ebb733
Nallans <nallans.code@gmail.com>
2026-08-21 21:19:57 +0200

    CS1E1

--- Consolidation_Set1/Exercise1/CS1E1.c
+++ Consolidation_Set1/Exercise1/CS1E1.c
@@ -1,3 +1,60 @@
+#include <stdio.h>
+
+void get_word(char* word);
+void get_letter(char* letter);
+int count_letter(char* word, char letter);
+
+int main(void)
+{
+    char word[30];
+    char letter = 'a';
+    int counter = 0;
+
+    get_word(word);
+    get_letter(&letter);
+    counter = count_letter(word, letter);
+    
+    if(counter == 0)
+    {
+        printf("The letter %c is not in this word, sorry :'(\n", letter);
+    }
+    else
+    {
+        printf("The letter %c appears %d times !\n", letter, counter);
+    }
+
+
+    return(0);
+}
+
+void get_word(char* word)
+{
+    printf("Word : ");
+    scanf("%s", word);
+}
+
+void get_letter(char* letter)
+{
+    printf("Letter : ");
+    scanf(" %c", letter);
+}
+
+int count_letter(char* word, char letter)
+{
+    int counter = 0, letter_counter = 0;
+
+    while(word[counter] != '\0')
+    {
+        if(word[counter] == letter)
+        {
+            letter_counter ++;
+        }
+        counter++;
+    }
+
+    return letter_counter;
+}
+
 /*
 Exercise 25 — Character counter
 
