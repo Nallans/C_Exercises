57260496df7faa8bb5fb17e8a612debd3593ebaf
Nallans <nallans.code@gmail.com>
2026-08-23 18:18:27 +0200

    CS1E2

--- Consolidation_Set1/Exercise2/CS1E2.c
+++ Consolidation_Set1/Exercise2/CS1E2.c
@@ -1,3 +1,47 @@
+#include <stdio.h>
+
+void get_word(char* word);
+void remove_vowels(char* word);
+
+int main(void)
+{
+    char word[30];
+
+    get_word(word);
+    remove_vowels(word);
+    printf("New word : %s\n", word);
+
+    return(0);
+}
+
+void get_word(char* word)
+{
+    printf("Original word : ");
+    scanf("%s", word);
+}
+
+void remove_vowels(char* word)
+{
+    int count = 0, count2 = 0;
+
+    while(word[count] != '\0')
+    {
+        while(word[count] == 'a' || word[count] == 'e' || word[count] == 'i' ||
+        word[count] == 'o' || word[count] == 'u' || word[count] == 'y' ||
+        word[count] == 'A' || word[count] == 'E' || word[count] == 'I' ||
+        word[count] == 'O' || word[count] == 'U' || word[count] == 'Y')
+        {
+            count2 = count;
+            while(word[count2] != '\0')
+            {
+                word[count2] = word[count2 + 1];
+                count2++;
+            }
+        }
+        count++;
+    }
+}
+
 /*
 Exercise 26 — Remove all vowels
 
