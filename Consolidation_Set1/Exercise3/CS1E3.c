48863a627e66a3808f820b8a0c78ba8602bd16eb
Nallans <nallans.code@gmail.com>
2026-08-23 18:43:25 +0200

    CS1E3

--- Consolidation_Set1/Exercise3/CS1E3.c
+++ Consolidation_Set1/Exercise3/CS1E3.c
@@ -1,3 +1,60 @@
+#include <stdio.h>
+
+void get_word(char* word);
+void get_letter(char* letter);
+int first_occurence(char* word, char letter);
+
+int main(void)
+{
+    char word[30];
+    char letter = 'a';
+    int count = 0;
+
+    get_word(word);
+    get_letter(&letter);
+
+    count = first_occurence(word, letter);
+    
+    if(count == 0)
+    {
+        printf("Sorry, the letter %c is not in this word :'(\n", letter);
+    }
+    else
+    {
+        printf("First occurence : index %d\n", count);
+    }
+
+    return(0);
+}
+
+void get_word(char* word)
+{
+    printf("Word : ");
+    scanf("%29s", word);
+}
+
+void get_letter(char* letter)
+{
+    printf("Letter : ");
+    scanf(" %c", letter);
+}
+
+int first_occurence(char* word, char letter)
+{
+    int count = 0;
+
+    while(word[count] != '\0')
+    {
+        if(word[count] == letter)
+        {
+            return count;
+            break;
+        }
+        count++;
+    }
+    return(0);
+}
+
 /*
 CS1E3 — Find the first occurrence
 
