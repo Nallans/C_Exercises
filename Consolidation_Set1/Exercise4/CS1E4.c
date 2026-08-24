f56742c9d81bfc66088f0694f2a7e42e1962ffc0
Nallans <nallans.code@gmail.com>
2026-08-24 09:55:43 +0200

    CS1E4

--- Consolidation_Set1/Exercise4/CS1E4.c
+++ Consolidation_Set1/Exercise4/CS1E4.c
@@ -1,3 +1,64 @@
+#include <stdio.h>
+
+void get_word(char* word);
+int get_length(char* word);
+void compare_lengths(int length1, int length2, char* word1, char* word2);
+
+int main(void)
+{
+    char word1[30], word2[30];
+    int length1 = 0, length2 = 0;
+    
+    printf("First word : ");
+    get_word(word1);
+    printf("Second word : ");
+    get_word(word2);
+    
+    printf( "The first word is : %s\n"
+            "The second word is : %s\n",
+            word1, word2);
+
+    length1 = get_length(word1);
+    length2 = get_length(word2);
+
+    compare_lengths(length1, length2, word1, word2);
+
+    return(0);
+}
+
+void get_word(char* word)
+{
+    scanf("%s", word);
+}
+
+int get_length(char* word)
+{
+    int counter = 0;
+
+    while(word[counter] != '\0')
+    {
+        counter++;
+    }
+
+    return counter ++;
+}
+
+void compare_lengths(int length1, int length2, char* word1, char* word2)
+{
+    if(length1 > length2)
+    {
+        printf("\"%s\" is longer.\n", word1);
+    }
+    else if(length2 > length1)
+    {
+        printf("\"%s\" is longer.\n", word2);
+    }
+    else
+    {
+        printf("The two words are the same size.\n");
+    }
+}
+
 /*
 Exercise 28 — Count words' lengths
 
