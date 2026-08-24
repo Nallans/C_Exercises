4c5b9f4fc1dba077091a08eb1244ed6eb4292c8d
Nallans <nallans.code@gmail.com>
2026-08-24 09:41:11 +0200

    CS1E4 Wording

---
+++ Consolidation_Set1/Exercise4/CS1E4.c
@@ -0,0 +1,16 @@
+/*
+Exercise 28 — Count words' lengths
+
+Ask the user for two words.
+
+Determine which one is longer.
+
+Example:
+
+First word: computer
+Second word: cat
+
+"computer" is longer.
+
+Then handle the case where they're the same length.
+*/
