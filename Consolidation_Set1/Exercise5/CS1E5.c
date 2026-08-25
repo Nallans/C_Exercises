f4c7661a178448cd17ba3f7fd0fc55be5ea6c7cb
Nallans <nallans.code@gmail.com>
2026-08-25 15:41:29 +0200

    CS1E5 testing version

--- Consolidation_Set1/Exercise5/CS1E5.c
+++ Consolidation_Set1/Exercise5/CS1E5.c
@@ -1,3 +1,51 @@
+#include <stdio.h>
+
+void get_word(char* word);
+int compare_words(char* word1, char* word2);
+
+int main(void)
+{
+    char word1[30], word2[30];
+    int result = 0;
+
+    printf("Word 1 : ");
+    get_word(word1);
+    printf("Word2 : ");
+    get_word(word2);
+
+    printf("Word 1 is %s, word 2 is %s\n", word1, word2);
+
+    result = compare_words(word1, word2);
+    printf("Result : %d\n", result);
+
+    return(0);
+}
+
+void get_word(char* word)
+{
+    scanf("%29s", word);
+}
+
+int compare_words(char* word1, char* word2)
+{
+    int count = 0;
+
+    while(word1[count] == word2[count])
+    {
+        printf(" %c %c |",word1[count], word2[count]);
+        count++;
+    }
+    if(word1[count] < word2[count])
+    {
+        return(-1);
+    }
+    else if(word1[count] > word2[count])
+    {
+        return(1);
+    }
+    else return(0);
+}
+
 /*
 Exercise 29 — Manual string comparison
 
