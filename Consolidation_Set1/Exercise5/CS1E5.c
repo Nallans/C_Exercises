9dc1d2b54654aeb5dfd447f5901914f1b81b1de0
Nallans <nallans.code@gmail.com>
2026-08-25 14:54:46 +0200

    CS1E5 Wording

---
+++ Consolidation_Set1/Exercise5/CS1E5.c
@@ -0,0 +1,21 @@
+/*
+Exercise 29 — Manual string comparison
+
+You've already made is_identical().
+
+Now make a function that returns:
+
+-1 if word 1 comes before word 2,
+0 if they're identical,
+1 if word 1 comes after word 2.
+
+For example:
+
+apple
+banana
+
+Result: -1
+
+Don't worry about making it identical to strcmp() yet. Just establish your own comparison logic.
+Comes before / after => In a dictionnary (lexographical order)
+*/
