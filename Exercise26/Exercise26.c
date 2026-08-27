262e01750fd1c0b5ed45e90607363093afb43228
Nallans <nallans.code@gmail.com>
2026-08-27 12:38:40 +0200

    Exercise 26

--- Exercise26/Exercise26.c
+++ Exercise26/Exercise26.c
@@ -1,3 +1,50 @@
+#include <stdio.h>
+
+void get_word(char* word);
+void remove_duplicates(char* word);
+
+int main(void)
+{
+    char word[30];
+
+    get_word(word);
+    remove_duplicates(word);
+    printf("Output : %s\n", word);
+
+
+    return(0);
+}
+
+void get_word(char* word)
+{
+    printf("Input : ");
+    scanf("%29s", word);
+}
+
+void remove_duplicates(char* word)
+{
+    int count1 = 0, count2 = 0, count3 = 0;
+
+    while(word[count1] != '\0')
+    {
+        count2 = count1 + 1;
+        while(word[count2] != '\0')
+        {
+            while(word[count2] == word[count1])
+            {
+                count3 = count2;
+                while(word[count3] != '\0')
+                {
+                    word[count3] = word[count3 + 1];
+                    count3++;
+                }
+            }
+            count2++;
+        }
+        count1++;
+    }
+}
+
 /*
 Exercise 26 — Remove duplicate characters
 
