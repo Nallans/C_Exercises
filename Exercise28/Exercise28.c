c3a017ba30d30867dc68c38b5cd7d9d83e46f85e
Nallans <nallans.code@gmail.com>
2026-08-31 13:39:13 +0200

    Exercise 28, first draft with occurence counting

--- Exercise28/Exercise28.c
+++ Exercise28/Exercise28.c
@@ -1,3 +1,55 @@
+#include <stdio.h>
+
+void get_word(char* word);
+void count_occurrences(char* word, int* occurrences);
+
+int main(void)
+{
+    char word[30];
+    int occurrences[30];
+
+    get_word(word);
+    printf("The word is %s\n", word);
+    count_occurrences(word, occurrences);
+    printf("The new word is %s\n", word);
+
+    return(0);
+}
+
+void get_word(char* word)
+{
+    printf("Input : ");
+    scanf("%29s", word);
+}
+
+void count_occurrences(char* word, int* occurrences)
+{
+    int count1 = 0, count2 = 0, count3 = 0, letter_count;
+
+    while(word[count1] != '\0')
+    {
+        count2 = count1 + 1;
+        letter_count = 1;
+        while(word[count2] != '\0')
+        {
+            while(word[count2] == word[count1])
+            {
+                count3 = count2;
+                letter_count++;
+                while(word[count3] != '\0')
+                {
+                    word[count3] = word[count3 + 1];
+                    count3++;
+                }
+            }
+            count2++;
+        }
+        occurrences[count1] = letter_count;
+        printf("Occurences for %c : %d\n", word[count1], occurrences[count1]);
+        count1++;
+    }
+}
+
 /*
 Exercise 28 — Find the most frequent character
 
