375ab0c521ec9ac75d785ca4f19352b3a5a41f58
Nallans <nallans.code@gmail.com>
2026-09-03 12:33:02 +0200

    Exercise 31 wording

---
+++ Exercise31/Exercise31.c
@@ -0,0 +1,15 @@
+/*
+Exercise 31 — Your First Structure
+
+Create a structure representing a person.
+
+It should contain:
+
+a name
+an age
+a height
+
+Create one person, ask the user for their information, then display it.
+
+Goal: Become comfortable declaring, creating, accessing, and modifying a structure.
+*/
