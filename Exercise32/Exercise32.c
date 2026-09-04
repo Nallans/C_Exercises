dee7784d07b07579d31100d66024284e6bbe5252
Nallans <nallans.code@gmail.com>
2026-09-04 22:40:40 +0200

    Exercise 32 Wording

---
+++ Exercise32/Exercise32.c
@@ -0,0 +1,14 @@
+/*
+Exercise 32 — Multiple Structures
+
+Create a program that stores information about 5 people.
+
+For each person, store:
+
+name
+age
+
+Ask the user for all 5 people and then display them.
+
+Your program should use an array of structures.
+*/
