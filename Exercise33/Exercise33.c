e34bb0b28da4f4e4584951c7f907e8033dbaabd5
Nallans <nallans.code@gmail.com>
2026-09-05 22:43:41 +0200

    Exercise 33

--- Exercise33/Exercise33.c
+++ Exercise33/Exercise33.c
@@ -1,3 +1,45 @@
+#include <stdio.h>
+
+struct People
+{
+    char name[20];
+    int age;
+};
+
+void get_informations(struct People* person);
+void display_informations(struct People* person);
+
+int main(void)
+{
+    struct People person[5];
+
+    get_informations(person);
+    display_informations(person);
+
+    return(0);
+}
+
+void get_informations(struct People* person)
+{
+    for(int i = 0; i < 5; i++)
+    {
+        printf("Person %d name : ", i + 1);
+        scanf("%19s", person[i].name);
+        printf("Person %d age : ", i + 1);
+        scanf("%d", &person[i].age);
+    }
+}
+
+void display_informations(struct People* person)
+{
+    for(int i = 0; i < 5; i++)
+    {
+        printf( "Person %d name : %s\n"
+                "Person %d age : %d\n",
+                i + 1, person[i].name, i + 1, person[i].age);
+    }
+}
+
 /*
 Exercise 33 — Functions and Structures
 
