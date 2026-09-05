514c516821a87b8b1a3e86b2b0a028fd7e4fd76c
Nallans <nallans.code@gmail.com>
2026-09-05 22:14:19 +0200

    Exercise 33 Wording

---
+++ Exercise33/Exercise33.c
@@ -0,0 +1,12 @@
+/*
+Exercise 33 — Functions and Structures
+
+Take Exercise 32 and modularize it.
+
+Create functions responsible for:
+
+filling the array
+displaying the array
+
+The functions should receive the appropriate data as parameters.
+*/
