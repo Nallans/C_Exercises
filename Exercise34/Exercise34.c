c1b3e84e1ea4587f2a0f05c6dbc393c7362e729d
Nallans <nallans.code@gmail.com>
2026-09-06 22:49:47 +0200

    Exercise 34, working structure

--- Exercise34/Exercise34.c
+++ Exercise34/Exercise34.c
@@ -1,3 +1,26 @@
+#include <stdio.h>
+
+struct People
+{
+    char name[20];
+    int age;
+};
+
+int main(void)
+{
+    struct People person[5] = {{"Bryan", 30}, {"James", 23}, {"Mary", 34},
+    {"John", 45}, {"Lea", 21}};
+
+    for(int i = 0; i < 5; i++)
+    {
+        printf( "Person %d name : %s\n"
+                "Person %d age : %d\n",
+                i + 1, person[i].name, i + 1, person[i].age);
+    }
+
+    return(0);
+}
+
 /*
 Exercise 34 — Searching Structures
 
