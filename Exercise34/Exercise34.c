2e66bf9d48286aaa5a5f420470b8a544ad5a7537
Nallans <nallans.code@gmail.com>
2026-09-06 22:36:42 +0200

    Exercise 34 Wording

---
+++ Exercise34/Exercise34.c
@@ -0,0 +1,13 @@
+/*
+Exercise 34 — Searching Structures
+
+Using an array of people, ask the user for a name.
+
+Search for that person.
+
+If found, display their information.
+
+If not found, say that the person doesn't exist.
+
+You should decide how your search function communicates its result.
+*/
