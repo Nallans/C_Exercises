eaa7d5790acd54a169c8d8dae416680ae906728c
Nallans <nallans.code@gmail.com>
2026-09-09 21:16:08 +0200

    Exercise 35 Wording

---
+++ Exercise35/Exercise35.c
@@ -0,0 +1,18 @@
+/*
+Exercise 35 — The Old Familiar Problem
+
+Create a structure representing a game:
+
+title
+release year
+rating
+
+Store 10 games.
+
+Then:
+
+Display all games.
+Find the game with the highest rating.
+Find the oldest game.
+Search for a game by title.
+*/
