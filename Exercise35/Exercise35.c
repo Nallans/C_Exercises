c6af46dfbcd791a61eac8b9549d4902593b3113d
Nallans <nallans.code@gmail.com>
2026-09-09 21:47:46 +0200

    Exercise 35, highest_rating function + changing games ratings

--- Exercise35/Exercise35.c
+++ Exercise35/Exercise35.c
@@ -7,16 +7,21 @@ struct Games
 };
 
 void display_games(struct Games* game);
+void highest_rating(struct Games* game);
 
 int main(void)
 {
-    struct Games game[10] = {{"Doom", 1993, 95},{"Doom 2", 1994, 98}, 
-    {"Half-Life", 1998, 98}, {"Half-Life 2", 2004, 98},
-    {"Duke Nukem 3D", 1996, 90}, {"Super Mario Bros", 1995, 98},
-    {"Castlevania", 1986, 90}, {"Ratchet & Clank", 2002, 90},
-    {"Halo", 2001, 95}, {"Pokemon", 1998, 95}};
+    struct Games game[10] = {{"Doom", 1993, 96},{"Doom 2", 1994, 100}, 
+    {"Half-Life", 1998, 97}, {"Half-Life 2", 2004, 98},
+    {"Duke Nukem 3D", 1996, 93}, {"Super Mario Bros", 1995, 99},
+    {"Castlevania", 1986, 92}, {"Ratchet & Clank", 2002, 91},
+    {"Halo", 2001, 95}, {"Pokemon", 1998, 94}};
 
     display_games(game);
+    printf("\n");
+
+    highest_rating(game);
+    printf("\n");
 
     return(0);
 }
@@ -28,6 +33,22 @@ void display_games(struct Games* game)
         printf( "Game : %s, Year : %d, Rating : %d / 100\n",
                 game[i].title, game[i].release_year, game[i].rating);
     }
+}
+
+void highest_rating(struct Games* game)
+{
+    int highest = 0, index = 0;
+
+    for(int i = 0; i < 10; i++)
+    {
+        if(game[i].rating > highest)
+        {
+            highest = game[i].rating;
+            index = i;
+        }
+    }
+    printf("Highest rated game is : %s with %d / 100 (%d)\n",
+        game[index].title, game[index].rating, game[index].release_year);
 }
 
 /*
