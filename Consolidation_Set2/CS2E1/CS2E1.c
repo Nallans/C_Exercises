efb5da677578a1a90c676e4745f367137cd077d2
Nallans <nallans.code@gmail.com>
2026-09-10 12:19:53 +0200

    CS2E1 Wording

---
+++ Consolidation_Set2/CS2E1/CS2E1.c
@@ -0,0 +1,20 @@
+/*
+CS2E1 — Student Records
+
+Create a structure representing a student.
+
+Each student has:
+
+a name
+an age
+a grade
+
+Store 5 students.
+
+Then:
+
+Display all students.
+Find the student with the highest grade.
+Find the youngest student.
+Ask for a name and search for that student.
+*/
