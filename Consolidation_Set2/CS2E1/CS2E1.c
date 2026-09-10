dbbf475f11f0c4bae62951af5d154adc263de20b
Nallans <nallans.code@gmail.com>
2026-09-10 12:26:25 +0200

    CS2E1 display_students function

--- Consolidation_Set2/CS2E1/CS2E1.c
+++ Consolidation_Set2/CS2E1/CS2E1.c
@@ -1,3 +1,34 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+struct Students
+{
+    char name[30];
+    int age, grade;
+};
+
+void display_students(struct Students* student);
+
+int main(void)
+{
+    struct Students student[5] = {{"John", 17, 80}, {"Lea", 16, 95},
+    {"Billy", 25, 78}, {"Lina", 20, 89}, {"Alex", 23, 56}};
+
+    display_students(student);
+    printf("\n");
+
+    return(0);
+}
+
+void display_students(struct Students* student)
+{
+    for(int i = 0; i < 5; i++)
+    {
+        printf("Student : %s, Age %d, Grade : %d\n",
+        student[i].name, student[i].age, student[i].grade);
+    }
+}
+
 /*
 CS2E1 — Student Records
 
