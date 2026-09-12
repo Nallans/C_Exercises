f54cd16ad6712e2173e4aac2019ba5fc88dec5a8
Nallans <nallans.code@gmail.com>
2026-09-12 21:38:35 +0200

    CS2E2 get_name function

--- Consolidation_Set2/CS2E2/CS2E2.c
+++ Consolidation_Set2/CS2E2/CS2E2.c
@@ -7,14 +7,21 @@ struct Games
 };
 
 void display_games(struct Games* game);
+void get_name(char* name);
 
 int main(void)
 {   
+    char name[30];
+
     struct Games game[5] = {{"Doom", 1993, 95},{"Pong", 1972, 80},
     {"Pacman", 1980, 85}, {"Super Mario Brothers", 1985, 98},
     {"Half-Life", 1998, 97}};
 
     display_games(game);
+    printf("\n");
+
+    get_name(name);
+    printf("You are looking for %s\n", name);
     printf("\n");
 
     return(0);
@@ -27,6 +34,12 @@ void display_games(struct Games* game)
         printf("%s, Year %d, Rated %d / 100\n",
         game[i].title, game[i].release_year, game[i].rating);
     }
+}
+
+void get_name(char* name)
+{
+    printf("What game are you looking for ? ");
+    scanf("%29[^\n]", name);
 }
 
 /*
