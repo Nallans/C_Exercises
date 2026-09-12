0553d80c298663910b2089069f2e79959c3d5aeb
Nallans <nallans.code@gmail.com>
2026-09-12 21:26:23 +0200

    CS2E2 Wording

---
+++ Consolidation_Set2/CS2E2/CS2E2.c
@@ -0,0 +1,19 @@
+/*
+CS2E2 — Game Rating Update
+
+Create an array containing 5 games.
+
+Each game has:
+
+title
+release year
+rating
+
+Display the games.
+
+Ask the user for a title.
+
+If the game exists, ask for a new rating and modify it.
+
+Finally, display the modified game.
+*/
