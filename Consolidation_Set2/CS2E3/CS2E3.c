3daa204616c473982966052caaeb784148ea290a
Nallans <nallans.code@gmail.com>
2026-09-14 13:50:16 +0200

    CS2E3 Wording

---
+++ Consolidation_Set2/CS2E3/CS2E3.c
@@ -0,0 +1,23 @@
+/*
+CS2E3 — Find the Extremes
+
+Create an array of 10 structures representing something of your choice.
+
+It could be:
+
+games
+books
+movies
+people
+hardware
+whatever you feel like.
+
+Each structure must contain at least three different pieces of information.
+
+Then find:
+
+the highest value of one property
+the lowest value of another property
+
+Display the complete structures corresponding to those results.
+*/
