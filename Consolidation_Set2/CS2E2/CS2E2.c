e436cf457fb522908db793f3b12a0be6f9a51276
Nallans <nallans.code@gmail.com>
2026-09-14 09:02:34 +0200

    CS2E2 case-insensitive

--- Consolidation_Set2/CS2E2/CS2E2.c
+++ Consolidation_Set2/CS2E2/CS2E2.c
@@ -52,7 +52,9 @@ bool string_compare(char* name1, char* name2)
 
     while(name1[count] != '\0' || name2[count] != '\0')
     {
-        if(name1[count] != name2[count])
+        if( name1[count] != name2[count] &&
+            name1[count] + 32 != name2[count] &&
+            name1[count] - 32 != name2[count])
         {
             return false;
         }
@@ -72,6 +74,7 @@ void modify_game(char* name, struct Games* game)
             is_found = true;
             printf("What is your rating of %s ? ", game[i].title);
             scanf("%d", &game[i].rating);
+
             while(game[i].rating < 0 || game[i].rating > 100)
             {
                 printf("Please give a rating between 0 and 100 : ");
