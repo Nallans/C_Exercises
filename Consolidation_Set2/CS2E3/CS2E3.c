59bd1257238145d0f378deec54619e8926209e2b
Nallans <nallans.code@gmail.com>
2026-09-14 13:58:06 +0200

    CS2E3 Struct initialized

--- Consolidation_Set2/CS2E3/CS2E3.c
+++ Consolidation_Set2/CS2E3/CS2E3.c
@@ -1,3 +1,22 @@
+#include <stdio.h>
+
+struct Games
+{
+    char title[30];
+    int release_year, rating;
+};
+
+int main(void)
+{
+    struct Games game[10] = {{"Doom", 1993, 95}, {"Doom 2", 1994, 97},
+    {"Pong", 1972, 80}, {"Super Mario Bros", 1985, 98},
+    {"Pacman", 1980, 87}, {"Half-Life", 1998, 94},
+    {"Half-Life 2", 2004, 96}, {"Ratchet & Clank", 2002, 92},
+    {"Jak & Daxter", 2001, 93}, {"SSX", 2000, 90}};
+
+    return(0);
+}
+
 /*
 CS2E3 — Find the Extremes
 
