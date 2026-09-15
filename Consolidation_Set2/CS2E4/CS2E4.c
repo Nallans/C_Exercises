f592ecf9284a23241c8bdbf4897e69d20627d954
Nallans <nallans.code@gmail.com>
2026-09-15 12:32:43 +0200

    CS2E4 Wording

---
+++ Consolidation_Set2/CS2E4/CS2E4.c
@@ -0,0 +1,25 @@
+/*
+CS2E4 — The Small Database
+
+This is the big one.
+
+Create a small program that stores 10 records of your own design.
+
+Your program must allow the user to:
+
+1. Display all records
+2. Search for a record
+3. Modify a record
+4. Display a maximum/minimum result
+5. Quit
+
+You decide:
+
+what the structure contains
+how the search works
+how modification works
+how the functions are organized
+what the menu looks like
+
+No prescribed architecture.
+*/
