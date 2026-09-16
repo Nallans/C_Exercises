8ef2e88cfe7333e842d95c01ac1faa803257231a
Nallans <nallans.code@gmail.com>
2026-09-16 20:51:07 +0200

    Exercise 37 Wording

---
+++ Exercise37/Exercise37.c
@@ -0,0 +1,11 @@
+/*
+Exercise 37 — Modify Through a Function
+
+Create a struct People again.
+
+Create a function that receives a pointer to a person and modifies their information.
+
+For example, the function could update the person's age and height.
+
+Your main() should create the person and call the function.
+*/
