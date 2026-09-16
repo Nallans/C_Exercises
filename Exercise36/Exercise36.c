17ce358a3dda143fdf1bac55a71de0ef6b34d2e6
Nallans <nallans.code@gmail.com>
2026-09-16 08:43:27 +0200

    Exercise 36 Wording

---
+++ Exercise36/Exercise36.c
@@ -0,0 +1,20 @@
+/*
+Exercise 36 — A Person Through a Pointer
+
+Create a struct People containing:
+
+name
+age
+height
+
+Create one person.
+
+Then create a pointer to that person.
+
+Use the pointer to:
+
+Display the person's information.
+Modify their age.
+Modify their height.
+Display the updated information.
+*/
