a022655b457d4777d1b8c5101deaf3c2678ff2c4
Nallans <nallans.code@gmail.com>
2026-09-17 08:08:14 +0200

    Exercise 38 Wording

---
+++ Exercise38/Exercise38.c
@@ -0,0 +1,15 @@
+/*
+Exercise 38 — Array of Structures, Modified Through a Function
+
+Create an array of 5 people.
+
+Then create separate functions to:
+
+display all people
+increase everyone's age by 1
+display everyone again
+
+The modification function should operate on the original array.
+
+Don't create another array just to perform the modification.
+*/
