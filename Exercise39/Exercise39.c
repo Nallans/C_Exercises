163af977c248905c37cf54ee1ef5a94bed4588fd
Nallans <nallans.code@gmail.com>
2026-09-18 12:24:10 +0200

    Exercise 39 structure initialized

--- Exercise39/Exercise39.c
+++ Exercise39/Exercise39.c
@@ -1,3 +1,23 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+struct Games
+{
+    char title[30];
+    int release_year, rating;
+};
+
+int main(void)
+{
+    struct Games game[10] = {{"Doom", 1993, 90}, {"Doom 2", 1994, 93},
+    {"Super Mario Bros", 1985, 87}, {"Ratchet and Clank", 2002, 92},
+    {"Jak & Daxter", 2001, 91}, {"SSX", 2000, 84},
+    {"Pong", 1972, 80}, {"Castlevania", 1986, 88},
+    {"Devil May Cry 5", 2019, 94}, {"Project Zomboid", 2013, 98}};
+
+    return(0);
+}
+
 /*
 Exercise 39 — Find and Modify
 
