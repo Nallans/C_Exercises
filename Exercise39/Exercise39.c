e5b423f0749beaf6310bfb582f0a21496c6a8169
Nallans <nallans.code@gmail.com>
2026-09-18 12:08:12 +0200

    Exercise 39 Wording

---
+++ Exercise39/Exercise39.c
@@ -0,0 +1,17 @@
+/*
+Exercise 39 — Find and Modify
+
+Create an array of 10 games.
+
+Each game contains:
+
+title
+release year
+rating
+
+Ask the user for a game title.
+
+If the game exists, allow the user to modify its rating.
+
+Otherwise, display an appropriate message.
+*/
