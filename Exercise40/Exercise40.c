1e2a2634228c1758ad040905288b17c7dbe37a84
Nallans <nallans.code@gmail.com>
2026-09-21 13:34:21 +0200

    Exercise 40 Wording

---
+++ Exercise40/Exercise40.c
@@ -0,0 +1,24 @@
+/*
+Exercise 40 — The Collection Manager
+
+This is the final exercise of the set.
+
+Create a small program managing 10 games.
+
+Each game contains:
+
+title
+release year
+rating
+
+The program should offer a simple menu:
+
+1. Display games
+2. Search for a game
+3. Change a game's rating
+4. Display highest-rated game
+5. Display oldest game
+6. Quit
+
+You decide how to organize the functions.
+*/
