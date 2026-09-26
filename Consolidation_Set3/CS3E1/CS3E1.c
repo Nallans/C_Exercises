f71f46fb86e4b8f16a6af342c54421d9c6a9ea4e
Nallans <nallans.code@gmail.com>
2026-09-26 21:45:19 +0200

    CS3E1 Wording

---
+++ Consolidation_Set3/CS3E1/CS3E1.c
@@ -0,0 +1,30 @@
+/*
+CS3E1 — Modify a Person
+
+Create:
+
+struct Person
+{
+    char name[30];
+    int age;
+    float height;
+};
+
+Create one person in main().
+
+Write:
+
+void birthday(struct Person *pPerson);
+
+The function should increase the person's age by 1.
+
+Then write:
+
+void grow(struct Person *pPerson);
+
+which increases the person's height by 0.01.
+
+Goal
+
+Call both functions from main() and display the person's information before and after.
+*/
